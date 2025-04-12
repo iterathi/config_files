f5300fa5448725c014d3c439359216002c2d2e0e
iterathi <contact@iterathi.com>
2025-04-12 02:54:29 -0400

    updated vscode, added fish abbrs and hyprland config

---
+++ fish/abbreviations.fish
@@ -0,0 +1,8 @@
+# open Hyprland config file
+abbr --add hyprconfig nano '~/.config/hypr/hyprland.conf'
+
+# open Hyprland keybindings config file
+abbr --add hyprkeys nano '~/.config/hypr/keybindings.conf'
+
+# open kitty config file
+abbr --add kittyconfig nano '~/.config/kitty/kitty.conf'
